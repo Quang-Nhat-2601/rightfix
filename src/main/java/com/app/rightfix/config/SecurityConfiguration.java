f1bb76b77542e5985874dc4adc7ae308d752260b
hongphat <hongphat1671998@gmail.com>
2024-06-15 00:19:54 +0700

    fix cros

--- src/main/java/com/app/rightfix/config/SecurityConfiguration.java
+++ src/main/java/com/app/rightfix/config/SecurityConfiguration.java
@@ -15,10 +15,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -56,14 +54,15 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+    public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");  // Allow any origin
+        config.addAllowedHeader("*");  // Allow any header
+        config.addAllowedMethod("*");  // Allow any method
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 
